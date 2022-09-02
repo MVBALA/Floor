79e10c7ab2ecdfb1f55509560f0aafa6e018ad77
MVBALA <43174220+MVBALA@users.noreply.github.com>
2022-09-03 01:12:03 +0530

    Update Floor.java

--- src/Floor.java
+++ src/Floor.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Floor {
+public class Floor {
+    private double width;
+    private double length;
+
+    public Floor(double width, double length) {
+        if (width < 0) {
+            this.width = 0;
+            this.length = length;
+        } else if (length < 0) {
+            this.width = width;
+            this.length = 0;
+
+        } else if (width < 0 && length < 0) {
+            this.width = 0;
+            this.length = 0;
+        } else {
+            this.width = width;
+            this.length = length;
+        }
+    }
+
+    public double getArea() {
+        return width * length;
+    }
 }
